c1bfece6f436eab6dec31cde8c698aaabf42ac7b
Ocean-Eyes <dlstlr52@naver.com>
2021-02-21 23:12:32 +0900

    Coding Test Day 7 Lost Training Wear

--- CodingTestDay1_StackQue_StockPrices/CodingTestDay3_DollDrawingMachine/Marathoner.cpp
+++ CodingTestDay1_StackQue_StockPrices/CodingTestDay3_DollDrawingMachine/Marathoner.cpp
@@ -1,72 +1,72 @@
-//#include <iostream>
-//#include <string>
-//#include <vector>
-//#include <algorithm>
-//#include <functional>
-//
-//using std::cout;
-//using std::endl;
-//using std::vector;
-//using std::string;
-//using std::greater;
-//
-//string solution(vector<string> participant, vector<string> completion);
-//
-//int main()
-//{
-//	vector<string> TestParticipant1 = { "leo", "kiki", "eden" };
-//	vector<string> TestCompletion1 = { "eden", "kiki" };
-//	vector<string> TestParticipant2 = { "marina", "josipa", "nikola", "vinko", "filipa" };
-//	vector<string> TestCompletion2 = { "josipa" , "filipa", "marina", "nikola" };
-//	vector<string> TestParticipant3 = { "mislav", "stanko", "mislav", "ana" };
-//	vector<string> TestCompletion3 = { "stanko", "ana", "mislav" };
-//	string TestReturn1;
-//	string TestReturn2;
-//	string TestReturn3;
-//
-//	TestReturn1 = solution(TestParticipant1, TestCompletion1);
-//	TestReturn2 = solution(TestParticipant2, TestCompletion2);
-//	TestReturn3 = solution(TestParticipant3, TestCompletion3);
-//
-//	cout << TestReturn1 << endl;
-//	cout << TestReturn2 << endl;
-//	cout << TestReturn3 << endl;
-//
-//	return 0;
-//}
-//
-//string solution(vector<string> participant, vector<string> completion) {
-//	string answer = "";
-//	string temp = "";
-//	string dummy = "";
-//
-//	sort(participant.begin(), participant.end(), greater<string>());
-//	sort(completion.begin(), completion.end(), greater<string>());
-//
-//	for (int w = 0; w < participant.size(); w++)
-//	{
-//		cout << participant[w] << " ";
-//	}
-//
-//	cout << endl;
-//
-//	for (int z = 0; z < completion.size(); z++)
-//	{
-//		cout << completion[z] << " ";
-//	}
-//
-//	cout << endl;
-//
-//	completion.push_back(dummy);
-//
-//	for (unsigned int k = 0; k < participant.size(); k++)
-//	{
-//		if (participant[k] != completion[k])
-//		{
-//			answer = participant[k];
-//			break;
-//		}
-//	}
-//
-//	return answer;
-//}
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+using std::greater;
+
+string solution(vector<string> participant, vector<string> completion);
+
+int main()
+{
+	vector<string> TestParticipant1 = { "leo", "kiki", "eden" };
+	vector<string> TestCompletion1 = { "eden", "kiki" };
+	vector<string> TestParticipant2 = { "marina", "josipa", "nikola", "vinko", "filipa" };
+	vector<string> TestCompletion2 = { "josipa" , "filipa", "marina", "nikola" };
+	vector<string> TestParticipant3 = { "mislav", "stanko", "mislav", "ana" };
+	vector<string> TestCompletion3 = { "stanko", "ana", "mislav" };
+	string TestReturn1;
+	string TestReturn2;
+	string TestReturn3;
+
+	TestReturn1 = solution(TestParticipant1, TestCompletion1);
+	TestReturn2 = solution(TestParticipant2, TestCompletion2);
+	TestReturn3 = solution(TestParticipant3, TestCompletion3);
+
+	cout << TestReturn1 << endl;
+	cout << TestReturn2 << endl;
+	cout << TestReturn3 << endl;
+
+	return 0;
+}
+
+string solution(vector<string> participant, vector<string> completion) {
+	string answer = "";
+	string temp = "";
+	string dummy = "";
+
+	sort(participant.begin(), participant.end(), greater<string>());
+	sort(completion.begin(), completion.end(), greater<string>());
+
+	for (int w = 0; w < participant.size(); w++)
+	{
+		cout << participant[w] << " ";
+	}
+
+	cout << endl;
+
+	for (int z = 0; z < completion.size(); z++)
+	{
+		cout << completion[z] << " ";
+	}
+
+	cout << endl;
+
+	completion.push_back(dummy);
+
+	for (unsigned int k = 0; k < participant.size(); k++)
+	{
+		if (participant[k] != completion[k])
+		{
+			answer = participant[k];
+			break;
+		}
+	}
+
+	return answer;
+}
--- CodingTestDay1_StackQue_StockPrices/CodingTestDay5_TrainingWear/TrainingWear.cpp
+++ CodingTestDay1_StackQue_StockPrices/CodingTestDay5_TrainingWear/TrainingWear.cpp
@@ -16,17 +16,20 @@ int main()
 	int n2 = 5;
 	int n3 = 3;
 	int n4 = 6;
+	int n5 = 5;
 
 	vector<int> TestLost1 = { 2,4 };
 	vector<int> TestLost2 = { 2,4 };
 	vector<int> TestLost3 = { 3 };
 	vector<int> TestLost4 = { 4,5 };
+	vector<int> TestLost5 = { 1,2,3 };
 	vector<int> TestReserve1 = { 1,3,5 };
 	vector<int> TestReserve2 = { 3 };
 	vector<int> TestReserve3 = { 1 };
 	vector<int> TestReserve4 = { 5,6 };
+	vector<int> TestReserve5 = { 1,2,3 };
 
-	int TestReturn1, TestReturn2, TestReturn3, TestReturn4;
+	int TestReturn1, TestReturn2, TestReturn3, TestReturn4, TestReturn5;
 
 	TestReturn1 = solution(n1, TestLost1, TestReserve1);
 	cout << "TestReturn1으로 반환" << endl;
@@ -36,11 +39,13 @@ int main()
 	cout << "TestReturn3으로 반환" << endl;
 	TestReturn4 = solution(n4, TestLost4, TestReserve4);
 	cout << "TestReturn4으로 반환" << endl;
+	TestReturn5 = solution(n5, TestLost5, TestReserve5);
 
 	cout << TestReturn1 << endl;
 	cout << TestReturn2 << endl;
 	cout << TestReturn3 << endl;
 	cout << TestReturn4 << endl;
+	cout << TestReturn5 << endl;
 
 	return 0;
 }
@@ -55,7 +60,7 @@ int solution(int n, vector<int> lost, vector<int> reserve) {
 	vector<int>::iterator IterPlus;
 
 
-	for (unsigned int i = 0; i < lost.size(); i++)
+	for (unsigned int i = 0; i < lost.size();)
 	{
 		IterOwn = find(reserve.begin(), reserve.end(), lost[i]);
 
@@ -67,9 +72,14 @@ int solution(int n, vector<int> lost, vector<int> reserve) {
 			reserve.erase(IterOwn);
 			count++;
 		}
+		// if an index of array is getting bigger when element is continuosly erased, it can be lost some cases. 
+		else if(IterOwn == reserve.end())
+		{
+			i++;
+		}
 	}
 
-	cout << "여벌을 가지고 온 학생이 도난당한 경우 모두 제거 후 lost의 크기는 " << lost.size() << endl;
+	cout << "여벌을 가지고 온 학생 본인이 도난당한 경우 모두 제거 후 lost의 크기는 " << lost.size() << endl;
 
 	for (unsigned int j = 0; j < lost.size(); j++)
 	{
