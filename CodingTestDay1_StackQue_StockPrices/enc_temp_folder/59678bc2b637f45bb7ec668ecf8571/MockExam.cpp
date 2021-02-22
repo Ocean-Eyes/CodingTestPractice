26eb52595e0cbe827be4a9c2e61fa132490d352e
Ocean-Eyes <dlstlr52@naver.com>
2021-02-22 21:49:25 +0900

    Coding Test Practice Day7 Mock Exam

---
+++ CodingTestDay1_StackQue_StockPrices/enc_temp_folder/59678bc2b637f45bb7ec668ecf8571/MockExam.cpp
@@ -0,0 +1,125 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using std::cout;
+using std::endl;
+using std::vector;
+using std::max_element;
+using std::min_element;
+using std::swap;
+
+vector<int> solution(vector<int> answers);
+
+int main()
+{
+	vector<int> TestAnswers1 = { 1,2,3,4,5 };
+	vector<int> TestAnswers2 = { 1,3,2,4,2 };
+	vector<int> TestAnswers3 = { 1,4,1,4,2 };
+	vector<int> TestAnswers4 = { 3,5,2,4,2,5,4,2,1,5,3,3,2 };
+	vector<int> TestReturn1, TestReturn2, TestReturn3, TestReturn4;
+
+	TestReturn1 = solution(TestAnswers1);
+	TestReturn2 = solution(TestAnswers2);
+	TestReturn3 = solution(TestAnswers3);
+	TestReturn4 = solution(TestAnswers4);
+
+	for (unsigned int l = 0; l < TestReturn1.size(); l++)
+	{
+		cout << TestReturn1[l] << " ";
+	}
+
+	cout << endl;
+	cout << "TestReturn1 출력" << endl;
+
+	for (unsigned int m = 0; m < TestReturn2.size(); m++)
+	{
+		cout << TestReturn2[m] << " ";
+	}
+
+	cout << endl;
+	cout << "TestReturn2 출력" << endl;
+
+	for (unsigned int n = 0; n < TestReturn3.size(); n++)
+	{
+		cout << TestReturn3[n] << " ";
+	}
+
+	cout << endl;
+	cout << "TestReturn3 출력" << endl;
+
+	for (unsigned int p = 0; p < TestReturn4.size(); p++)
+	{
+		cout << TestReturn4[p] << " ";
+	}
+
+	cout << endl;
+	cout << "TestReturn4 출력" << endl;
+
+	return 0;
+}
+
+vector<int> solution(vector<int> answers) {
+	vector<int> answer;
+	vector<int> student1 = { 1,2,3,4,5 };
+	vector<int> student2 = { 2,1,2,3,2,4,2,5,2 };
+	vector<int> student3 = { 3,3,1,1,2,2,4,4,5,5 };
+	vector<int> count = { 0, 0, 0 };
+	int max_index, min_index;
+
+	for (unsigned int i = 0; i < answers.size(); i++)
+	{
+		if (answers[i] == student1[i % 5])
+		{
+			count[0]++;
+		}
+
+		if (answers[i] == student2[i % 9])
+		{
+			count[1]++;
+		}
+
+		if (answers[i] == student3[i % 10])
+		{
+			count[2]++;
+		}
+	}
+	
+	max_index = max_element(count.begin(), count.end()) - count.begin();
+
+	// There's no maximum scores or There are 2 or 3 same maximum scores.
+	if(max_index != *count.end())
+	{
+		// All students get same scores.
+		if ((count[0] == count[1]) && (count[1] == count[2]) && (count[2] == count[0]))
+		{
+			answer = { 1, 2, 3 };
+		}
+		// 2 students get same maximum scores.
+		else
+		{
+			for (unsigned int j = 0; j < count.size(); j++)
+			{
+				for (unsigned int k = 0; k < count.size() - 1; k++)
+				{
+					if (count[j] > count[k + 1])
+					{
+						answer.push_back(j+1);
+					}
+					else
+					{
+						answer.push_back(k+2);
+					}
+				}
+			}
+		}
+	}
+	// There's one maximum scores.
+	else
+	{
+		answer.push_back(max_index + 1);
+	}
+
+	return answer;
+}
